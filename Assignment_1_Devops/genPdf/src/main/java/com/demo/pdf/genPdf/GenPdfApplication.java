ce9a7069ecfed1631019c5ab83d137f1da182b19
GargiParab <gargi.parab57@gmail.com>
2022-11-10 18:05:29 +0530

    To show conflicts Gargi

--- Assignment_1_Devops/genPdf/src/main/java/com/demo/pdf/genPdf/GenPdfApplication.java
+++ Assignment_1_Devops/genPdf/src/main/java/com/demo/pdf/genPdf/GenPdfApplication.java
@@ -1,14 +1,13 @@
 package com.demo.pdf.genPdf;
-
+import java.util.lang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class GenPdfApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GenPdfApplication.class, args);
-		System.out.println("gen pdf");
+		// SpringApplication.run(GenPdfApplication.class, args);
+		System.out.println("gen pdf Modified");
 	}
 
 }
