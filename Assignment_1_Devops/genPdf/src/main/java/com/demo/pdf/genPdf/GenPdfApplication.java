805f6086fa046c6985c58e7eb75d17a5fc981a9c
GargiParab <gargi.parab57@gmail.com>
2022-11-13 10:37:45 +0530

    Force commit Gargi

--- Assignment_1_Devops/genPdf/src/main/java/com/demo/pdf/genPdf/GenPdfApplication.java
+++ Assignment_1_Devops/genPdf/src/main/java/com/demo/pdf/genPdf/GenPdfApplication.java
@@ -8,7 +8,7 @@ public class GenPdfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenPdfApplication.class, args);
-		System.out.println("Force commit3");
+		System.out.println("Force commit general");
 	}
 
 }
